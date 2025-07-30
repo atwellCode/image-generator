25dd9dc74ebf71ad6e2f2968f3c42716a7a2f55a
Arslan <businessatwellcode@gmail.com>
2025-07-30 14:17:07 +0500

    all Pricing page is added

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { assets, plans } from '../assets/assets'
 
 const BuyCredit = () => {
   return (
-    <div>
-      But Credit Page
+    <div className='min-h-[80vh] text-center pt-14 mb-10'>
+      <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans</button>
+      <h1 className='text-center text-3xl font-medium mb-6 sm:mb-10'>Choose The Plans</h1>
+
+      <div className='flex flex-wrap justify-center gap-6 text-left'>
+        {plans.map((item, index) =>(
+            <div key={index}
+            className='bg-white shadow-md border rounded-lg py-12 px-8 text-gray-600 hover:scale-105 transition-all duration-400'
+            >
+                <img width={40} src={assets.logo_icon} alt='Lock-Icon'/>
+                <p className='mt-3 mb-1 font-semibold'>{item.id}</p>
+                <p className='text-sm'>{item.desc}</p>
+                <p className='mt-6 mb-8'><span className='text-3xl font-medium'>
+                   ${item.price} </span>  / {item.credits} Credits</p>
+                   <button className='w-full bg-gray-800 text-white text-sm  rounded-md py-2.5 min-w-52'>Buy Now</button>
+            </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const Result = () => {
-  return (
-    <div>
-      Result Page
-    </div>
-  )
-}
+  const [image, setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState('');
 
-export default Result
+  const onSubmitHandler = async(e)=>{
+    alert('form is woiking');
+  }
+  return (
+    <form onSubmit={onSubmitHandler} className="flex flex-col min-h-[90vh] justify-center items-center">
+      <div>
+        <div className="relative">
+          <img
+            className="max-w-sm rounded-md"
+            src={image}
+            alt={assets.sample_img_1}
+          />
+          <span className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${loading ? 'w-full transition-all duration-[10s]' : 'w-0'}`} />
+        </div>
+        <p className={!loading ? 'hidden' : ''}>Loading....</p>
+      </div>
+      {!isImageLoaded && 
+        <div className="flex w-full  max-w-xl bg-neutral-500 text-white text-sm p-0.5  mt-10 rounded-full ">
+          <input
+          onChange={e => setInput(e.target.value)} value={input}
+            className="flex-1 px-3 bg-transparent outline-none ml-8  max-sm:w-20"
+            type="text"
+            placeholder="Describe what you want to Generate."
+          />
+          <button
+            type="submit"
+            className="bg-zinc-800 px-10 sm:px-16 py-3 rounded-full"
+          >
+            Generate
+          </button>
+        </div>
+      }
+
+      {isImageLoaded && 
+      <div className="flex gap-2 justify-center text-white text-sm p-0.5 mt-10 rounded-full">
+        <p onClick={() =>{setIsImageLoaded(false)}} className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer">
+          Generate Another
+        </p>
+        <div className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer">
+          <a href={image} download>
+            Download
+          </a>
+        </div>
+      </div>}
+    </form>
+  );
+};
+
+export default Result;
